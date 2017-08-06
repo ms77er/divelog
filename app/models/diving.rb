936f8b48e56a8a5909cb898358f7e9fe8e0837c0
M.Seto <ms77er@gmail.com>
2017-08-06 05:04:58 +0000

    validation

--- app/models/diving.rb
+++ app/models/diving.rb
@@ -1,4 +1,13 @@
 class Diving < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
+  
+  validates :DiveNo, length: { maximum: 10 }
+  validates :location, length: { maximum: 255 }
+  validates :MaxDepth, length: { maximum: 3 }  
+  validates :AvgDepth, length: { maximum: 3 }  
+  validates :WaterTemp, length: { maximum: 3 }    
+  validates :Transparancy, length: { maximum: 3 }    
+  validates :Memo, length: { maximum: 255 }   
+  
 end
